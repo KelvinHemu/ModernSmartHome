ac115e0a87f3a1dc387f56c25be6fbae0ebdcba4
KelvinHemu <kelvinhemu@gmail.com>
2024-10-17 20:42:47 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/morden_smart_home/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.morden_smart_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:morden_smart_home/util/smart_device_box.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // padding constants
+  final double horizontalPadding = 40;
+  final double verticalPadding = 25;
+
+  // list of smart devices
+  List mySmartDevices = [
+    // [ smartDeviceName, iconPath , powerStatus ]
+    ["Smart Light", "lib/icons/light-bulb.png", true],
+    ["Smart AC", "lib/icons/air-conditioner.png", false],
+    ["Smart TV", "lib/icons/smart-tv.png", false],
+    ["Smart Fan", "lib/icons/fan.png", false],
+  ];
+  // power button switched
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      mySmartDevices[index][2] = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //custom App Bar
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: horizontalPadding, vertical: verticalPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //menu icon
+                  Image.asset(
+                    'lib/icons/menu.png',
+                    height: 45,
+                    color: Colors.grey[800],
+                  ),
+
+                  Icon(
+                    Icons.person,
+                    size: 45,
+                    color: Colors.grey[800],
+                  )
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            //Welcome Home Kelvin Hemu
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding,
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome Home',
+                    style: TextStyle(
+                        fontSize: 18, color: Color.fromARGB(255, 15, 15, 15)),
+                  ),
+                  Text(
+                    'Kelvin Hemu',
+                    style: TextStyle(fontSize: 40),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            //smart devices + grid
+
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: const Text("Smart Devices"),
+            ),
+            Expanded(
+                child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1 / 1.12,
+              ),
+              itemCount: 4,
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              itemBuilder: (context, index) {
+                return SmartDeviceBox(
+                  smartDeviceName: mySmartDevices[index][0],
+                  iconPath: mySmartDevices[index][1],
+                  powerOn: mySmartDevices[index][2],
+                  onChanged: (value) => powerSwitchChanged(value, index),
+                );
+              },
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
